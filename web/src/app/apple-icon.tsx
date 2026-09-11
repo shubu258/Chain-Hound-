96d51834546f5c0597fec743c7d723198a15ab26
shubu258 <shubu2581@gmail.com>
2026-09-11 18:25:08 +0530

    fevicon

---
+++ web/src/app/apple-icon.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 180, height: 180 };
+export const contentType = "image/png";
+
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#C15A28",
+          color: "#FFFFFF",
+          fontSize: 120,
+          fontWeight: 700,
+          fontFamily: "Georgia, serif",
+        }}
+      >
+        C
+      </div>
+    ),
+    { ...size },
+  );
+}
